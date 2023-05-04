fe5000030113823d87ade4d6eebc91b579981ee9
Wludzik, Jozef <jozef.wludzik@intel.com>
2023-05-04 19:35:57 +0200

    Add option to build OpenVINO package
    Enable building OpenVINO package together with VPUX plugin. Moved all
    compiler stuff to compiler directory.
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ compiler/compiler_source.cmake
@@ -0,0 +1,54 @@
+# Copyright 2022-2023 Intel Corporation.
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were
+# provided to you ("License"). Unless the License provides otherwise, you may
+# not use, modify, copy, publish, distribute, disclose or transmit this
+# software or the related documents without Intel's prior written permission.
+#
+# This software and the related documents are provided as is, with no express
+# or implied warranties, other than those that are expressly stated in
+# the License.
+
+if(TARGET vpux_plugin_source)
+  return()
+endif()
+
+include(ExternalProject)
+
+# OpenVINO + VPUX Plugin package options
+set(OPENVINO_REVISION 0a5ca5375265f0f12cdaee68574030408dd1c352)
+set(VPUX_PLUGIN_REVISION vpu_chrome_alpha_rc1)
+set(VPUX_PLUGIN_RELEASE vpu_chrome_alpha_rc1)
+
+# Directories
+set(OPENVINO_PREFIX_DIR "${CMAKE_BINARY_DIR}/third_party/openvino")
+set(OPENVINO_SOURCE_DIR "${OPENVINO_PREFIX_DIR}/src/openvino")
+file(MAKE_DIRECTORY ${OPENVINO_SOURCE_DIR})
+
+set(VPUX_PLUGIN_PREFIX_DIR "${CMAKE_BINARY_DIR}/third_party/vpux_plugin")
+set(VPUX_PLUGIN_SOURCE_DIR "${VPUX_PLUGIN_PREFIX_DIR}/src/vpux_plugin")
+file(MAKE_DIRECTORY ${VPUX_PLUGIN_SOURCE_DIR})
+
+ExternalProject_Add(
+  openvino_source
+  GIT_REPOSITORY https://github.com/openvinotoolkit/openvino.git
+  GIT_TAG ${OPENVINO_REVISION}
+  PREFIX ${OPENVINO_PREFIX_DIR}
+  SOURCE_DIR ${OPENVINO_SOURCE_DIR}
+  PATCH_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND "")
+
+ExternalProject_Add(
+  vpux_plugin_source
+  GIT_REPOSITORY https://github.com/openvinotoolkit/vpux_plugin.git
+  GIT_TAG ${VPUX_PLUGIN_REVISION}
+  PREFIX ${VPUX_PLUGIN_PREFIX_DIR}
+  SOURCE_DIR ${VPUX_PLUGIN_SOURCE_DIR}
+  PATCH_COMMAND
+    git -C ${VPUX_PLUGIN_SOURCE_DIR}/thirdparty/vpucostmodel lfs pull
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND "")
---
+++ compiler/vpux_compiler.cmake
@@ -0,0 +1,36 @@
+# Copyright 2022-2023 Intel Corporation.
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were
+# provided to you ("License"). Unless the License provides otherwise, you may
+# not use, modify, copy, publish, distribute, disclose or transmit this
+# software or the related documents without Intel's prior written permission.
+#
+# This software and the related documents are provided as is, with no express
+# or implied warranties, other than those that are expressly stated in
+# the License.
+
+if(NOT ENABLE_VPUX_COMPILER)
+  return()
+endif()
+
+if(EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/vpux_compiler_cache.cmake AND ENABLE_VPUX_COMPILER_CACHE)
+  include(vpux_compiler_cache.cmake)
+else()
+  include(vpux_compiler_build.cmake)
+endif()
+
+add_library(vpux_compiler SHARED IMPORTED GLOBAL)
+add_dependencies(vpux_compiler ${VPUX_COMPILER_DEPENDENCY})
+
+set_target_properties(vpux_compiler PROPERTIES
+  IMPORTED_LOCATION ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/libVPUXCompilerL0.so
+  IMPORTED_LINK_DEPENDENT_LIBRARIES ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/libtbb.so.2)
+
+target_include_directories(vpux_compiler INTERFACE ${VPUX_COMPILER_INCLUDE_DIR})
+target_compile_definitions(vpux_compiler INTERFACE ENABLE_VPUX_COMPILER)
+
+install(FILES
+  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/libVPUXCompilerL0.so
+  ${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/libtbb.so.2
+  TYPE LIB)
