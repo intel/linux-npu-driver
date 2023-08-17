82a42d13cb651062dfefe64d31cf3fc2188e894b
Wludzik, Jozef <jozef.wludzik@intel.com>
2023-08-17 15:27:29 +0200

    Allow to compile driver with installed level-zero
    Now driver can be compiled with level-zero that is installed in the
    system. Moved the level_zero headers preparation out of
    umd/level_zero_driver/CMakelists.txt to third_party. Moved the level
    zero tests to third_party
    
    Signed-off-by: Wludzik, Jozef <jozef.wludzik@intel.com>

---
+++ third_party/cmake/level-zero-vpu-extensions.cmake
@@ -0,0 +1,15 @@
+# Copyright 2022-2023 Intel Corporation.
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were
+# provided to you ("License"). Unless the License provides otherwise, you may
+# not use, modify, copy, publish, distribute, disclose or transmit this software
+# or the related documents without Intel's prior written permission.
+#
+# This software and the related documents are provided as is, with no express or
+# implied warranties, other than those that are expressly stated in the License.
+
+set(LEVEL_ZERO_EXT_HEADERS_DIR "${CMAKE_BINARY_DIR}/include/level_zero")
+file(MAKE_DIRECTORY ${LEVEL_ZERO_EXT_HEADERS_DIR})
+file(GLOB_RECURSE LEVEL_ZERO_HEADERS ${CMAKE_CURRENT_SOURCE_DIR}/level-zero-vpu-extensions/*.h)
+file(COPY ${LEVEL_ZERO_HEADERS} DESTINATION ${LEVEL_ZERO_EXT_HEADERS_DIR})
