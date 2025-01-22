28bf4aec934f62286d005c4c983cf0f96f47c2ed
Schulfer, Magdalena <magdalena.schulfer@intel.com>
2025-01-22 13:14:24 +0100

    CMake changes for npu-1.13.0 drop
    Signed-off-by: Schulfer, Magdalena <magdalena.schulfer@intel.com>

---
+++ third_party/cmake/perfetto.cmake
@@ -0,0 +1,22 @@
+# Copyright 2024 Intel Corporation.
+#
+# This software and the related documents are Intel copyrighted materials, and
+# your use of them is governed by the express license under which they were
+# provided to you ("License"). Unless the License provides otherwise, you may
+# not use, modify, copy, publish, distribute, disclose or transmit this software
+# or the related documents without Intel's prior written permission.
+#
+# This software and the related documents are provided as is, with no express or
+# implied warranties, other than those that are expressly stated in the License.
+
+
+project(perfetto)
+find_package(Threads)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+add_library(perfetto STATIC "${CMAKE_CURRENT_SOURCE_DIR}/perfetto/sdk/perfetto.cc")
+target_include_directories(perfetto INTERFACE "${CMAKE_CURRENT_SOURCE_DIR}/perfetto/sdk")
+
+set_property(TARGET perfetto PROPERTY POSITION_INDEPENDENT_CODE ON)
