40bfec3a377996548a8d5e1bbc2acc68bbb6c713
Atlas Yu <pseudoc@163.com>
2025-02-19 15:17:17 +0800

    build(nix): use the standard flake output

--- firmware/default.nix
+++ firmware/default.nix
@@ -1,22 +1,35 @@
-{ lib, stdenv, stack-version, ... }:
-stdenv.mkDerivation {
-  pname = "intel-npu-linux-firmware";
-  version = stack-version;
+{
+  lib,
+  stdenvNoCC,
+  version,
+  system,
+  ...
+}:
+
+stdenvNoCC.mkDerivation {
+  pname = "intel-npu-firmware";
+  version = version;
 
   src = with lib.fileset; toSource {
-    root = ./.;
-    fileset = ./bin;
+    root = ../.;
+    fileset = unions [
+      ./bin
+      ../LICENSE.md
+    ];
   };
 
   meta = {
     homepage = "https://github.com/intel/linux-npu-driver";
     description = "Intel NPU (Neural Processing Unit) firmware";
-    platforms = [ "x86_64-linux" ];
+    platforms = [ system ];
     license = lib.licenses.mit;
   };
 
   installPhase = ''
     mkdir -p $out/lib/firmware/intel/vpu
-    cp -P bin/*.bin $out/lib/firmware/intel/vpu
+    cp -P firmware/bin/*.bin $out/lib/firmware/intel/vpu
+
+    mkdir -p $out/share/doc/intel-npu-driver
+    cp LICENSE.md $out/share/doc/intel-npu-driver/LICENSE
   '';
 }
